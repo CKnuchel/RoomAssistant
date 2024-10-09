d75ea8d6a2b838945ef70a6e6bca76a782d13450
CKnuchel <christoph.knuchel@gmail.com>
2024-10-09 18:12:01 +0200

    ADDED: Retrofit2.gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.room.common)
     implementation(libs.androidx.room.ktx)
     implementation(libs.retrofit2)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
